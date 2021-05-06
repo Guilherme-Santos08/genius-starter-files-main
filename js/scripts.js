c9025c6d27ad044118c4835eca06781a37d2edeb
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-05-06 15:43:13 -0300

    aula 3/8 finalizada

--- js/scripts.js
+++ js/scripts.js
@@ -42,10 +42,17 @@ _gui.switch.addEventListener("click", () => {
 	_data.score = 0
 	_data.gameSequence = []
 	_data.playerSequence = []
+
+	disablePads();
+
+	_gui.led.classList.remove("gui__led--active")
 });
 
 _gui.strict.addEventListener("click", () => {
-	_gui.led.classList.toggle("gui__led--active")
+	if(!_data.gameOn){
+		return
+	}
+	_data.strict = _gui.led.classList.toggle("gui__led--active")
 });
 
 _gui.start.addEventListener("click", () => {
@@ -93,5 +100,7 @@ const changePadCursor = (cursorType) => {
 }
 
 const disablePads = () => {
-
+	_gui.pads.forEach(pad => {
+		pad.classList.remove("game__pad--active");
+	});
 }
