cbb763b73a95f367c3d7ddd2d28d50fc9a9a760e
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-06-05 22:14:53 -0300

    JS

--- js/scripts.js
+++ js/scripts.js
@@ -84,7 +84,33 @@ const newColor = () => {
    setScore();
 };
 
-const playSequence = () => {};
+const playSequence = () => {
+   let counter = 0,
+      padOn = true;
+
+   _data.playerSequence = [];
+   _data.playerCanPlay = false;
+
+   const interval = setInterval(() => {
+      if (padOn) {
+         if(counter === _data.gameSequence.length) {
+            clearInterval(interval);
+            disablePads();
+            _data.playerCanPlay = true;
+            return;
+         }
+         const sndId = _data.gameSequence[counter];
+         const pad = _gui.pads[sndId];
+
+         _data.sounds[sndId].play();
+         pad.classList.add("game__pad--active");
+      } else {
+         disablePads();
+      }
+
+      padOn = !padOn;
+   }, 750);
+};
 
 const blink = (text, callback) => {
    let counter = 0,
